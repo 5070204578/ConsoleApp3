76f98763be06f2485d36db024ec5ed868ca76c70
MONSTER <MONSTER@DESKTOP-FORJ0TP>
2023-12-21 17:00:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+namespace ConsoleApp3
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "kendra", "scoot", "felipe" };
+            
+            names.Add("maria");
+            names.Add("bill");
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+
+            }
+            Console.WriteLine($"My name is {names[0]}");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+            Console.WriteLine($"The list has {names.Count} people in it");
+
+            var index = names.IndexOf("Felipe");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+            }
+
+            index = names.IndexOf("Not Found");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            }
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+    }
+}
